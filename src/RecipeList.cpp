e633cdd997dc7fb321b7432cb80e1a269a18591c
jtmilleer <jtmilleer05@gmail.com>
2025-08-08 15:31:47 -0500

    added more methods to RecipeList

--- src/RecipeList.cpp
+++ src/RecipeList.cpp
@@ -50,7 +50,7 @@ int RecipeList::getSize() const{
     return this->size;
 }
 
-Recipe RecipeList::getIndex(const int &index) const  {
+Recipe RecipeList::getIndex(const int &index) const{
     if (index < 0 || index >= size) {
         throw std::out_of_range("Index out of range");
     }
@@ -69,3 +69,72 @@ Recipe RecipeList::getIndex(const int &index) const  {
     }
     return tmp->data;
 }
+
+Recipe RecipeList::removeIndex(const int &index) {
+    if (index < 0 || index >= size) {
+        throw std::out_of_range("Index out of range");
+    }
+    Node *tmp = nullptr;
+    if((this->size / 2) < index){ //the index is past the halfway point, start at back and go backwards
+        tmp = tail;
+        for(int i = this->size - 1; i > index; --i){
+            tmp = tmp->prev;
+        }
+    }
+    else{ //start and beginning and go forwards
+        tmp = head;
+        for(int i = 0; i < index; ++i){
+            tmp = tmp->next;
+        }
+    }
+
+    //remove node from list then delete node
+    tmp->next->prev = tmp->prev;
+    tmp->prev->next = tmp->next;
+    tmp->next = nullptr;
+    tmp->prev = nullptr;
+    Recipe tmpRecipe = tmp->data;
+    delete tmp;
+    --this->size;
+    return tmpRecipe;
+}
+
+void RecipeList::insert(const int &index, const Recipe &input) {
+    if (index < 0 || index >= size) {
+        throw std::out_of_range("Index out of range");
+    }
+    if (index - 1 == this->size) { //at end of list can just append
+        this->append(input);
+    }
+    if (index == 0) {
+        Node *newNode = new Node;
+        newNode->data = input;
+        newNode->next = this->head;
+        this->head->prev = newNode;
+        this->head = newNode;
+        ++this->size;
+    }
+    Node *tmp = nullptr;
+    if((this->size / 2) < index){ //the index is past the halfway point, start at back and go backwards
+        tmp = tail;
+        for(int i = this->size - 1; i > index; --i){
+            tmp = tmp->prev;
+        }
+    }
+    else{ //start and beginning and go forwards
+        tmp = head;
+        for(int i = 0; i < index; ++i){
+            tmp = tmp->next;
+        }
+    }
+
+    Node *newNode = new Node;
+    newNode->data = input;
+    //tie in new node
+    tmp->prev->next = newNode;
+    newNode->prev = tmp->prev;
+    tmp->prev = newNode;
+    newNode->next = tmp;
+    ++this->size;
+}
+
--- src/RecipeList.h
+++ src/RecipeList.h
@@ -15,5 +15,8 @@ class RecipeList{
         void clear();
         int getSize() const;
         Recipe getIndex(const int &index) const;
+        Recipe removeIndex(const int &index);
+        void insert(const int &index, const Recipe &input);
+
 
 };
